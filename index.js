c62ad9f6b924f53c15d7b5136207e49de834d565
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-06-20 22:09:45 -0700

    prev/next button functionality

--- index.js
+++ index.js
@@ -3,6 +3,20 @@ let imgWidth = parseInt(window.getComputedStyle(document.querySelector(".contain
 let totalWidth = parseInt(window.getComputedStyle(document.querySelector(".container")).getPropertyValue("width"));
 
 let container = document.getElementsByClassName("container")[0];
+const containerStyle = window.getComputedStyle(container);
 
 console.log(imgWidth);
 console.log(totalWidth);
+
+function prevImage() {
+    let currentMargin = parseInt(containerStyle.marginLeft);
+    container.style.marginLeft = Math.min(currentMargin + imgWidth, 0).toString() + "px";
+}
+
+function nextImage() {
+    let currentMargin = parseInt(containerStyle.marginLeft);
+    container.style.marginLeft = Math.max(currentMargin - imgWidth, 0 - totalWidth + imgWidth).toString() + "px";
+}
+
+const prevButton = document.getElementsByClassName("previous")[0].addEventListener("click", prevImage);
+const nextButton = document.getElementsByClassName("next")[0].addEventListener("click", nextImage);
