00431d1fe949f37d1de5c3b9c6bb0251548482c5
VolTorian <33360939+VolTorian@users.noreply.github.com>
2024-06-26 01:56:55 -0700

    images working with webpack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 import ImageCarousel from './ImageCarousel.js';
+import barberry from './images/barberry.png';
+import chilli from './images/chilli.png';
+import pepper from './images/pepper.png';
+import saffron from './images/saffron.png';
 
-const test = new ImageCarousel("test-carousel", ["./images/barberry.png", "./images/chilli.png", "./images/pepper.png", "./images/saffron.png"]);
+const test = new ImageCarousel("test-carousel", [barberry, chilli, pepper, saffron]);
